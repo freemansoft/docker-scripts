0cb5a38ab9ea6fd240cd2823ed1dd2e4f7febf90
Joe Freeman <joe+github@freemansoft.com>
2020-03-17 19:40:30 -0400

    Add some comments and spacing

--- dask/samples/quickstart.py
+++ dask/samples/quickstart.py
@@ -7,8 +7,11 @@ def square(x):
 def neg(x):
     return -x
 
+# This will run two different distributed operations
 A = client.map(square, range(100))
 B = client.map(neg, A)
+
 total = client.submit(sum, B)
 total.result()
+
 client.gather(A)
